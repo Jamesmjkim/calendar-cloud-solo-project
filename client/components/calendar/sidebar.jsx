83a10be37f5deaa9a212b733da7245a6939a4fe6
James Kim <james.minjae.97@gmail.com>
2021-12-13 01:01:20 -0500

    finished sidebar

--- client/components/calendar/sidebar.jsx
+++ client/components/calendar/sidebar.jsx
@@ -4,11 +4,11 @@ const Sidebar = () => {
   return (
     <div
       className='container col-2'
-      style={{ height: '100vh', backgroundColor: '#F0F8FF' }}
+      style={{ width: '15vw', height: '100vh', backgroundColor: '#F0F8FF' }}
     >
       <div
-        className='d-flex flex-column justify-content-start'
-        style={{ width: '100vw' }}
+        className='d-flex flex-column justify-content-start my-4'
+        style={{ width: '14vw', height: '15vh' }}
       >
         <a className='navbar-brand' href='/'>
           <img src='http://localhost:3000/static/logo.png' width='50px' />
@@ -16,6 +16,32 @@ const Sidebar = () => {
         <a className='navbar-brand fs-4 text-black' href='/'>
           Calendar Cloud
         </a>
+      </div>
+      <div
+        className='container d-flex flex-column mx-2'
+        style={{ height: '20vh' }}
+      >
+        <a className='navbar-brand fs-4 text-black' href='/homepage'>
+          Home
+        </a>
+        <a className='navbar-brand fs-4 text-black' href='/events'>
+          Events
+        </a>
+        <a className='navbar-brand fs-4 text-black' href='/files'>
+          Files
+        </a>
+        <a className='navbar-brand fs-4 text-black' href='/settings'>
+          Settings
+        </a>
+      </div>
+      <div
+        className='container d-flex flex-column justify-content-end mx-auto'
+        style={{ height: '60vh' }}
+      >
+        <p className='fs-5'>quotes from api</p>
+        <p className='text-muted' style={{ fontSize: '12px' }}>
+          Website created by James Kim
+        </p>
       </div>
     </div>
   );
