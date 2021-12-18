f0ae6665387775c84862730ddd2957fa942a4566
James Kim <james.minjae.97@gmail.com>
2021-12-18 13:53:30 -0500

    database edit

--- server/models/userDataModel.js
+++ server/models/userDataModel.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://localhost/calendar-cloud';
+const MONGO_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/calendar-cloud';
 
-mongoose.connect(MONGO_URI);
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'calendarCloud',
+  })
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch((err) => console.log(err));
+
 const Schema = mongoose.Schema;
 
 const userDataSchema = new Schema({
