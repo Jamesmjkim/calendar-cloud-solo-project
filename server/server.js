f95ffd5691ec6c88ab5f8353604b92db97445a82
James Kim <james.minjae.97@gmail.com>
2021-12-18 14:02:30 -0500

    deployment changes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const fileUpload = require('express-fileupload');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
